d84853c019f4630859d65ed51599211833c9fbe3
Alessandro Campo <alessandrocampo97@gmail.com>
2024-03-18 17:15:05 +0100

    filled databse w seeder

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,8 +1,35 @@
 @extends('layouts.main')
 @section('title', 'DC Comics|Home')
 
+<?php
+$arr_keys = array_keys($comics[0]);
+
+?>
+
 @section('content')
-    <h2 class="text-danger ">
-        yooooooooooooooooooooooooooo
-    </h2>
+    <table class="table table-dark text-center">
+        <thead>
+            <tr>
+                <th scope="col">#</th>
+                @foreach ($arr_keys as $key)
+                    <th>
+                        {{ strtoupper($key) }}
+                    </th>
+                @endforeach
+            </tr>
+        </thead>
+        <tbody>
+
+            @foreach ($comics as $comic)
+                <tr>
+                    <th scope="row" class="px-5">{{ $loop->iteration }}</th>
+                    @foreach ($comic as $key => $value)
+                        <td>{{ $value }}</td>
+                    @endforeach
+                </tr>
+            @endforeach
+
+
+        </tbody>
+    </table>
 @endsection
